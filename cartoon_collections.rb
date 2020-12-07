fec17b1146dcbf868e982d098c81bc08c662bd52
Chip Lempke <clempke90@gmail.com>
2020-12-07 21:42:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,6 +4,7 @@ def greet_characters(array)
   array.each do |element|
   puts "Hello #{element}!"
 end
+end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
